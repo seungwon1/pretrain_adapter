3dc3e03e7c7a6783c43c3e4926e00608bad8ff86
Seungwon Kim <pennymagic156@gmail.com>
2020-12-22 21:17:39 +0900

    update pretrain

--- pretrain/dont_stop_pretraining/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from dont_stop_pretraining.models.basic_classifier_with_f1 import BasicClassifierWithF1
--- pretrain/scripts/mk_log.sh
+++
@@ -1,6 +0,0 @@
-log=$1
-dest=$2
-mkdir -p $dest
-rm $log/model.tar.gz
-rm $log/best.th
-cp -r $log $dest
